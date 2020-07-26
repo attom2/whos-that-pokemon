2f575590cd3df0605ed540a57795737bab5a4909
Andy Tom <andrew.tom@colorado.edu>
2020-07-26 15:14:51 -0600

    destructures variables in pokedex

--- src/Components/PokeDetails/PokeDetails.js
+++ src/Components/PokeDetails/PokeDetails.js
@@ -2,28 +2,24 @@ import React from 'react';
 import './PokeDetails.scss';
 
 const PokeDetails = ({details, isShiny}) => {
-  const pokemonName = details.name;
-  const pokemonType = details.types.map(poke => poke.type.name + '\n');
-  const pokeFrontImage = details.sprites.front_default;
-  const pokeBackImage = details.sprites.back_default;
-  const pokeBackShiny = details.sprites.back_shiny;
-  const pokeFrontShiny = details.sprites.front_shiny;
-  console.log(details);
+  const {sprites, name, types} = details;
+  const {front_default, back_default, front_shiny, back_shiny} = sprites;
+  const pokemonType = types.map(poke => poke.type.name + '\n');
   return (
     <figure>
       <section className="img-container">
         <img
           className='pokemon-pic'
-          src={isShiny ? pokeFrontShiny: pokeFrontImage}
-          alt={pokemonName}
+          src={isShiny ? front_shiny : front_default}
+          alt={name}
         />
         <img
           className='pokemon-pic'
-          src={isShiny ? pokeBackShiny :pokeBackImage}
-          alt={pokemonName}
+          src={isShiny ? back_shiny : back_default}
+          alt={name}
         />
       </section>
-      <h2>{pokemonName}</h2>
+      <h2>{name}</h2>
       <figcaption>
         <p>Height: {Math.round(parseInt(details.height)*10)/100}m</p>
         <p>Weight: {Math.round(parseInt(details.weight)*10)/100}kg</p>
