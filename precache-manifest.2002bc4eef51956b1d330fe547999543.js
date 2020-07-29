dfaf68862f223d9313f2d6a2f18e0608e78313f2
Andy Tom <andrew.tom@colorado.edu>
2020-07-28 18:52:20 -0600

    Updates

---
+++ precache-manifest.2002bc4eef51956b1d330fe547999543.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c99231e8c0c5ffac9ca80aa36ae674",
+    "url": "/whos-that-pokemon/index.html"
+  },
+  {
+    "revision": "032e0197f8fbb00d45c7",
+    "url": "/whos-that-pokemon/static/css/main.bd0f4ca5.chunk.css"
+  },
+  {
+    "revision": "57c238aaf19f538f74b9",
+    "url": "/whos-that-pokemon/static/js/2.5296fc7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/whos-that-pokemon/static/js/2.5296fc7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032e0197f8fbb00d45c7",
+    "url": "/whos-that-pokemon/static/js/main.a8d1fc31.chunk.js"
+  },
+  {
+    "revision": "ee58db62f8c5cabe6246",
+    "url": "/whos-that-pokemon/static/js/runtime-main.052cbded.js"
+  },
+  {
+    "revision": "c3b1756b172e6c39418f7386b2d9a6dd",
+    "url": "/whos-that-pokemon/static/media/EndCall3.c3b1756b.mp3"
+  },
+  {
+    "revision": "b5f3b04dca5fac4f2a2334b56fed0ff7",
+    "url": "/whos-that-pokemon/static/media/HollowBellNotification.b5f3b04d.mp3"
+  },
+  {
+    "revision": "d749ab74b876d5c52256c0559b487aff",
+    "url": "/whos-that-pokemon/static/media/Pokemon Hollow.d749ab74.woff"
+  },
+  {
+    "revision": "4197aed63a267f9ae61f800cda823803",
+    "url": "/whos-that-pokemon/static/media/Pokemon Solid.4197aed6.woff"
+  },
+  {
+    "revision": "a684eaf35d56f9d39cbc3a2aa52b0db8",
+    "url": "/whos-that-pokemon/static/media/whos-that-pokemon_.a684eaf3.mp3"
+  }
+]);
